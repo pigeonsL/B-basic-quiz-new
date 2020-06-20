2e8e4eff3ee8b5817d208786444561d01373778c
JingeLi <jinge.li@thoughtworks.com>
2020-06-20 11:39:20 +0800

    refactor Use the annotation replacement password format check method

--- src/main/java/com/thoughtworks/capacity/gtb/mvc/UserService.java
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/UserService.java
@@ -1,12 +1,9 @@
 package com.thoughtworks.capacity.gtb.mvc;
 
-import com.thoughtworks.capacity.gtb.mvc.exception.UserNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 @Service
 public class UserService {
@@ -17,32 +14,18 @@ public class UserService {
         userMap.put(2, new User(2, "username2", "username_2", "username2@mail.com"));
     }
 
-    public void addUser(User user) throws UserNotFoundException {
-        if(!IsValidPasswd(user.getPasswd()))
-            throw new UserNotFoundException("密码不合法");
+    public void addUser(User user)  {
         int count = userMap.size() + 1;
         user.setId(count);
         userMap.put(user.getId(), user);
     }
 
-    public User login(String name, String passwd) throws UserNotFoundException {
-        if(IsValidPasswd(passwd)){
-            for(User user: userMap.values()){
-                if(user.getName().equals(name) && user.getPasswd().equals(passwd)){
-                    return user;
-                }
+    public User login(String name, String passwd) {
+        for(User user: userMap.values()){
+            if(user.getName().equals(name) && user.getPasswd().equals(passwd)){
+                return user;
             }
         }
-        throw new UserNotFoundException("用户名或密码错误");
-    }
-    public boolean IsValidPasswd(String passwd){
-        if(passwd.length() >= 5 && passwd.length() <= 12){
-            Pattern pt = Pattern.compile("^\\w+$");
-            Matcher mt = pt.matcher(passwd);
-            if(mt.matches()){
-                return true;
-            }
-        }
-        return false;
+        return null;
     }
 }
